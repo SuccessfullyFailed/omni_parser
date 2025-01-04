d6cbefed64ed02b80d5e797b89279272538d3229
SuccessfullyFailed <60329202+RenzoGerritzen@users.noreply.github.com>
2025-01-04 03:49:27 +0100

    NestedCore move include_unmatched setting to NestedCodeParser struct

--- src/core/nested/nested_code_parser.rs
+++ src/core/nested/nested_code_parser.rs
@@ -10,7 +10,8 @@ pub const UNMATCHED_SEGMENT_NAME:&str = "UNNAMED";
 
 
 pub struct NestedCodeParser {
-	identification:Vec<SegmentIdentification>
+	identification:Vec<SegmentIdentification>,
+	include_unmatched:bool
 }
 impl NestedCodeParser {
 	
@@ -19,49 +20,23 @@ impl NestedCodeParser {
 	/// Create a new parser.
 	pub fn new(identification:Vec<&dyn SegmentIdentificationSource>) -> NestedCodeParser {
 		NestedCodeParser {
-			identification: identification.iter().map(|id_source| id_source.to_identification()).collect::<Vec<SegmentIdentification>>()
+			identification: identification.iter().map(|id_source| id_source.to_identification()).collect::<Vec<SegmentIdentification>>(),
+			include_unmatched: false
 		}
+	}
+
+	/// Return a version of self that includes unmatched contents.
+	pub fn include_unmatched(mut self) -> Self {
+		self.include_unmatched = true;
+		self
 	}
 
 
 
 	/* USAGE METHODS */
 
-	/// Parse a piece of code and create a debug document.
-	pub fn parse_debug(&self, contents:&str, output_file_path:&str) -> Result<(), Box<dyn Error>> {
-		let results:Vec<NestedCode<'_>> = self.parse(contents, false);
-
-		// Prepare segment-names debug display.
-		let mut names:Vec<String> = self.identification.iter().map(|ident| ident.name().to_string()).collect::<Vec<String>>();
-		names.dedup();
-
-		// Create debug display file.
-		let mut debug_code:String = String::new();
-		let mut injections:Vec<(usize, Option<String>)> = results.iter().map(|result| [(result.start(), Some(result.type_name().to_string())), (result.end(), None)]).flatten().collect::<Vec<(usize, Option<String>)>>();
-		injections.sort_by(|a, b| a.0.cmp(&b.0));
-		let mut last_injection_position:usize = 0;
-		for (position, name) in injections {
-			let mut contents_split:String = contents[last_injection_position..position].to_string();
-			for (find, replace) in HTML_REPLACEMENTS {
-				contents_split = contents_split.replace(find, &replace);
-			}
-			debug_code += &contents_split;
-			debug_code += &match name { Some(name) => format!("<span class=\"identifier {name}\">"), None => "</span>".to_string() };
-			last_injection_position = position;
-		}
-		debug_code += &contents[last_injection_position..];
-		debug_code += "\n<style>\n";
-		debug_code += "\t.spacer { margin-left: 20px; }";
-		debug_code += "\t.identifier { color: #0FF000; background-color: #0000FF; border: solid black 1px; }";
-		debug_code += &names.iter().enumerate().map(|(shift, name)| format!("\t.{name} {} filter: hue-rotate({}deg); {}", '{', (shift as f32 / names.len() as f32 * 360.0) as usize, '}')).collect::<Vec<String>>().join("\n");
-		debug_code += "\n</style>";
-
-		// Write debug code to file.
-		FileRef::new(&output_file_path).write(&debug_code)
-	}
-
 	/// Parse a piece of code.
-	pub fn parse<'a>(&self, contents:&'a str, include_unmatched:bool) -> Vec<NestedCode<'a>> {
+	pub fn parse<'a>(&self, contents:&'a str) -> Vec<NestedCode<'a>> {
 		let mut depth:Vec<&SegmentIdentification> = Vec::new();
 		let mut results:Vec<NestedCode> = Vec::new();
 
@@ -76,7 +51,7 @@ impl NestedCodeParser {
 			if allow_sub_parse {
 				for identification_set in &self.identification {
 					if !found_anything && Self::tag_matches_contents(&contents, cursor, identification_set.open(), identification_set.open_escape()) {
-						if include_unmatched && depth.is_empty() && last_unmatched_cursor != cursor {
+						if self.include_unmatched && depth.is_empty() && last_unmatched_cursor != cursor {
 							results.push(NestedCode::new(&contents, last_unmatched_cursor..cursor, 0, UNMATCHED_SEGMENT_NAME));
 							last_unmatched_cursor = cursor;
 						}
@@ -115,12 +90,45 @@ impl NestedCodeParser {
 					result.set_end(cursor);
 				}
 			}
-		} else if include_unmatched && last_unmatched_cursor != cursor {
+		} else if self.include_unmatched && last_unmatched_cursor != cursor {
 			results.push(NestedCode::new(&contents, last_unmatched_cursor..cursor, 0, UNMATCHED_SEGMENT_NAME));
 		}
 
 		// Return results.
 		results
+	}
+
+	/// Parse a piece of code and create a debug document.
+	pub fn parse_debug_file(&self, contents:&str, output_file_path:&str) -> Result<(), Box<dyn Error>> {
+		let results:Vec<NestedCode<'_>> = self.parse(contents);
+
+		// Prepare segment-names debug display.
+		let mut names:Vec<String> = self.identification.iter().map(|ident| ident.name().to_string()).collect::<Vec<String>>();
+		names.dedup();
+
+		// Create debug display file.
+		let mut debug_code:String = String::new();
+		let mut injections:Vec<(usize, Option<String>)> = results.iter().map(|result| [(result.start(), Some(result.type_name().to_string())), (result.end(), None)]).flatten().collect::<Vec<(usize, Option<String>)>>();
+		injections.sort_by(|a, b| a.0.cmp(&b.0));
+		let mut last_injection_position:usize = 0;
+		for (position, name) in injections {
+			let mut contents_split:String = contents[last_injection_position..position].to_string();
+			for (find, replace) in HTML_REPLACEMENTS {
+				contents_split = contents_split.replace(find, &replace);
+			}
+			debug_code += &contents_split;
+			debug_code += &match name { Some(name) => format!("<span class=\"identifier {name}\">"), None => "</span>".to_string() };
+			last_injection_position = position;
+		}
+		debug_code += &contents[last_injection_position..];
+		debug_code += "\n<style>\n";
+		debug_code += "\t.spacer { margin-left: 20px; }";
+		debug_code += "\t.identifier { color: #0FF000; background-color: #0000FF; border: solid black 1px; }";
+		debug_code += &names.iter().enumerate().map(|(shift, name)| format!("\t.{name} {} filter: hue-rotate({}deg); {}", '{', (shift as f32 / names.len() as f32 * 360.0) as usize, '}')).collect::<Vec<String>>().join("\n");
+		debug_code += "\n</style>";
+
+		// Write debug code to file.
+		FileRef::new(&output_file_path).write(&debug_code)
 	}
 
 	/// Check if a specific tag matches a specific place in contents.
